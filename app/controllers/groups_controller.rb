0cf6b8f02888a051b658cf7f088060541bfb5bee
zhouwenming <wmingphone@gmail.com>
2017-02-19 20:20:28 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,7 +27,15 @@ end
      @group.update(group_params)
 
      redirect_to groups_path, notice: "Update Success"
-  end   
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
+       
 private
 
 def group_params
